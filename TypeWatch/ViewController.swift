6a5d6fb46fa850bf7e2c8b180bd1879abc0cf5ec
= <=>
2017-12-08 22:58:11 -0500

    Initial Commit

---
+++ TypeWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TypeWatch
+//
+//  Created by Brendon Ho on 12/8/17.
+//  Copyright © 2017 Mengo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
